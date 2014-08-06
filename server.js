29de222627433ab652484458409ef381c05be185
Sriprabha <shri7287@gmail.com>
2014-08-05 21:55:04 -0400

    updating the listen port for heroku

--- server.js
+++ server.js
@@ -88,5 +88,5 @@
 
 
 	// listen (start app with node server.js) ======================================
-	app.listen(8080);
+	app.listen(process.env.PORT || 5000);
 	console.log("App listening on port 8080");
